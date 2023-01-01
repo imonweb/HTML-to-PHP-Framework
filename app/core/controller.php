979a3e29cf7f4b84a812513c06c060111d34286a
Imon <contact@imonweb.co.uk>
2023-01-01 12:12:09 +0000

    router

--- app/core/controller.php
+++ app/core/controller.php
@@ -11,4 +11,16 @@ class Controller
       include "../app/views/404.php";
     }
   }
+
+  protected function loadModel($model)
+  {
+     if(file_exists("../app/models/". $model .".php"))
+    {
+      include "../app/models/". $model .".php";
+      return $model = new $model();
+    }  
+
+    return false;
+  }
+
 }
